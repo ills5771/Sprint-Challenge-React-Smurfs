2a429c56c71633354400fb53e87ee08a9cb14954
ills5771 <ilyayelizarov1@gmail.com>
2019-03-01 08:51:53 -0800

    added two routes,one for SmurfForm and one for Smurfs

--- village/src/App.js
+++ village/src/App.js
@@ -3,6 +3,7 @@ import axios from "axios";
 import "./App.css";
 import SmurfForm from "./components/SmurfForm";
 import Smurfs from "./components/Smurfs";
+import { Route } from "react-router-dom";
 
 class App extends Component {
   constructor(props) {
@@ -29,8 +30,12 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <SmurfForm />
-        <Smurfs smurfs={this.state.smurfs} />
+        <Route path="/" component={SmurfForm} />
+        <Route
+          exact
+          path="/"
+          render={props => <Smurfs {...props} smurfs={this.state.smurfs} />}
+        />
       </div>
     );
   }
